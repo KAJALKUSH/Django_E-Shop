ac3a17fa06b7dc4691b20141389b60b5ac6bb6ec
Kajal Kushwaha <kajalkushwaha1210@gmail.com>
2024-07-16 16:08:16 +0530

    initial commit

--- store/views/checkout.py
+++ store/views/checkout.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.hashers import  check_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import check_password
 from store.models.customer import Customer
 from django.views import View
 from store.models.product import Product
 from store.models.orders import Order
+
 
 class CheckOut(View):
     def post(self, request):
@@ -12,15 +13,15 @@ class CheckOut(View):
         customer = request.session.get('customer')
         cart = request.session.get('cart')
         products = Product.get_products_by_id(list(cart.keys()))
-        print(address,phone,customer,products)
+        print(address, phone, customer, products)
 
         for product in products:
-            order = Order(customer = Customer(id = customer),
-                          product = product,
-                          price = product.price,
-                          address = address,
-                          phone = phone,
-                          quantity = cart.get(str(product.id)))
+            order = Order(customer=Customer(id=customer),
+                          product=product,
+                          price=product.price,
+                          address=address,
+                          phone=phone,
+                          quantity=cart.get(str(product.id)))
             order.save()
         request.session['cart'] = {}
 
